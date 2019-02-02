e9e62ceb313119eba9ee3ab4db2c901594e1ac27
Tom Richards <tom@tomrichards.net>
2019-02-02 12:48:30 -0500

    Save coverage artifacts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+# Code coverage
 require 'simplecov'
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
 SimpleCov.start
 
 require 'jekyll'
