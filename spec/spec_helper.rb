e1e2e9c651f94d953d67ea48d8f021b0a49cdcdb
Tom Richards <tom@tomrichards.net>
2020-11-10 10:13:52 -0500

    Fix coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,18 +2,21 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
-require 'simplecov'
-SimpleCov.start do
-  enable_coverage :branch
-end
+if Process.argv0 != 'bin/mutant'
+  require 'simplecov'
+  SimpleCov.start do
+    enable_coverage :branch
+    minimum_coverage line: 100, branch: 100
+  end
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  formatters = [
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::Codecov
-  ]
-  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(formatters)
+  if ENV['CI']
+    require 'codecov'
+    formatters = [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::Codecov
+    ]
+    SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(formatters)
+  end
 end
 
 require 'rspec'
